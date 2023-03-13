e5501a12e9b84e2a495895ba09f961e044577e16
Dmitrii <Dokter322@yandex.ru>
2023-03-13 18:41:19 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import {useCallback, useState} from "react";
 import {Editable, Slate, withReact} from "slate-react";
-import {createEditor, Editor, Transforms, Text} from "slate";
+import {createEditor, Transforms, Text, Editor} from "slate";
+const initialValue = [
+      {
+        type: "paragraph",
+        children: [{ text: 'A line of text in a paragraph.' }],
+      }
+]
 
 const CustomEditor = {
   toggleBoldMarkSpecial(editor) {
@@ -14,43 +20,33 @@ const CustomEditor = {
   },
 }
 
-const initialValue = [
-      {
-        type: "paragraph",
-        children: [{ text: 'A line of text in a paragraph.' }],
-      }
-]
-
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()))
-
   const renderLeaf = useCallback(props => {
     return <Leaf {...props} />
   }, [])
 
-  return <Slate onChange={(e) => {
-  }} editor={editor} value={initialValue}>
-    <Editable
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-            const [match] = Editor.nodes(editor, {at: [0],
-              match: n => /\{([^}]+)\}/g.test(n.text) && !n.bold,
+  return <Slate onChange={() => {
+    const [match] = Editor.nodes(editor, {at: [0],
+              match: n => /\{[^{}]*\}/g.test(n.text) && !n.bold,
               mode: "all"
-              })
+          })
             if(match){
-              console.log(match)
               const pos = match[0].text.split("").findIndex((e) => e === "{")
               const pos2 = match[0].text.split("").findIndex((e) => e === "}")
               Transforms.select(editor, {
                 anchor: Editor.start(editor, {path: match[1], offset: pos}),
                 focus: Editor.end(editor, {
-                  path: [0,0],
+                  path: match[1],
                   offset: pos2 + 1
                 }),
               })
             CustomEditor.toggleBoldMarkSpecial(editor, pos2)
             }
-        }}
+  }} editor={editor} value={initialValue}>
+    <Editable
+        renderLeaf={renderLeaf}
+        }
     />
   </Slate>
 }
