c2b94c1a5b5778948ec518ce45d0c86fef19157d
Dokter3228 <dmitrii.kovalev.dk@yandex.ru>
2023-03-22 00:07:03 +0300

    add some more interesting functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {Editable, Slate, withReact} from "slate-react";
 import {createEditor, Transforms, Text, Editor} from "slate";
 const initialValue = [
       {
         type: "paragraph",
-        children: [{ text: 'A line of text in a paragraph.' }],
+        children: [{ text: 'A line of tex{fadsfasdf} t in  {fasfasf}a  {fasdf}p {Fdsafsdf}ar {fsafd}agr {fasdfas}ap {fdasfasf}h.' }],
       }
 ]
 
 const CustomEditor = {
-  toggleBoldMarkSpecial(editor) {
+  toggleBoldMarkSpecial(editor, pos2) {
     Transforms.setNodes(
       editor, {
           bold: true,
         },
       { match: n => Text.isText(n), split: true }
     )
-    Transforms.deselect(editor)
   },
 }
 
@@ -26,8 +25,32 @@ const App = () => {
     return <Leaf {...props} />
   }, [])
 
-  return <Slate onChange={() => {
+  useEffect(() => {
     const [match] = Editor.nodes(editor, {at: [0],
+      match: n => /\{[^{}]*\}/g.test(n.text) && !n.bold,
+      mode: "all"
+    })
+    if(match){
+      const pos = match[0].text.split("").findIndex((e) => e === "{")
+      const pos2 = match[0].text.split("").findIndex((e) => e === "}")
+      Transforms.select(editor, {
+        anchor: Editor.start(editor, {path: match[1], offset: pos}),
+        focus: Editor.end(editor, {
+          path: match[1],
+          offset: pos2 + 1
+        }),
+      })
+      CustomEditor.toggleBoldMarkSpecial(editor, pos2)
+      console.log(editor.getFragment())
+      Transforms.select(editor, {
+        anchor: editor.selection.focus,
+        focus: editor.selection.focus,
+      } )
+    }
+  }, [])
+
+  return <Slate onChange={() => {
+      const [match] = Editor.nodes(editor, {at: [0],
               match: n => /\{[^{}]*\}/g.test(n.text) && !n.bold,
               mode: "all"
           })
@@ -41,7 +64,12 @@ const App = () => {
                   offset: pos2 + 1
                 }),
               })
-            CustomEditor.toggleBoldMarkSpecial(editor, pos2)
+              CustomEditor.toggleBoldMarkSpecial(editor, pos2)
+                console.log(editor.getFragment())
+              Transforms.select(editor, {
+                anchor: editor.selection.focus,
+                focus: editor.selection.focus,
+              } )
             }
   }} editor={editor} value={initialValue}>
     <Editable
